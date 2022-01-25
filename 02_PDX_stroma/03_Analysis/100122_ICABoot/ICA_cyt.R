d975776979843e95af126a39515ccc1f1fffb57f
j-solor <jacobo.solorzano@inserm.fr>
2022-01-25 17:04:17 +0100

    GSVA analysis + reorganization of the script

--- 02_PDX_stroma/03_Analysis/100122_ICABoot/ICA_cyt.R
+++ 02_PDX_stroma/03_Analysis/100122_ICABoot/ICA_cyt.R
@@ -5,8 +5,10 @@ library(JADE)
 library(corrplot)
 library(Hmisc)
 library(pheatmap)
-library("ggpubr")
-library("ggplotify")
+library(ggpubr)
+library(ggplotify)
+library("msigdbr")
+library(GSVA)
 ################################################################################
 setwd("/home/jacobo/Documents/02_TRANSDUCER/02_PDX_stroma/03_Analysis/100122_ICABoot/")
 source("functions.R")
@@ -50,11 +52,7 @@ cyt__ %>% rownames_to_column("EnsemblID") %>%
   dplyr::filter(EnsemblID %in% names(mostvar)) %>%
   column_to_rownames("EnsemblID") -> cyt_icaready
 
-# celltype deconvolution (unfiltered data)
-
-
-# ICA
-## Bootstrapping
+# ICA Bootstrapping
 if (run_boot == TRUE){
   ### Baseline before bootstrap
   cyt_icaready %>% jade_range(range.comp, MARGIN = 1) -> base_res_gene
@@ -76,26 +74,24 @@ if (run_boot == TRUE){
   boot_plots(s_boot = sample_boot, g_boot = gene_boot, name = "Cyt")
 } 
 
-## Most robust n.comp analysis
+# Most robust ICA analysis
 elected_ncomp <- 7 
-#elected_ncomp <- 710
-### Run best n.comp ICA
+#elected_ncomp <- 10
+
 jade_result <- JADE(cyt_icaready, n.comp = elected_ncomp)
 colnames(jade_result[["A"]]) <- paste("IC", 1:elected_ncomp, sep = ".")
 rownames(jade_result[["A"]]) <- names(jade_result$Xmu)
 
-### Component distribution analysis
+# Sample weight analysis
+## Metadata
 A_mat <- as.data.frame(jade_result[["A"]])
 S_mat <- as.data.frame(jade_result[["S"]])
 annotations <- sample_info[-1]
 stopifnot(rownames(A_mat) == rownames(annotations))
 plot_sample_weights(A_mat, annotations)
 
-### metadata
-#### Corplot
 corr_continuous <- annotations %>% dplyr::select(!Diabetes) %>% bind_cols(A_mat)
 corr_continuous <- corr_continuous[rownames(A_mat),] # merge mess with the order
-
 
 continuous_rcorr <- rcorr(data.matrix(corr_continuous), type = "spearman")
 continuous_rcorr$r <- continuous_rcorr$r[colnames(annotations %>% dplyr::select(!Diabetes)),colnames(A_mat)]
@@ -103,26 +99,29 @@ continuous_rcorr$P <- continuous_rcorr$P[colnames(annotations %>% dplyr::select(
 
 corrplot(continuous_rcorr$r,
          p.mat = continuous_rcorr$P, sig.level = 0.05, insig = "blank")
+
+### Export for further correlations
 stopifnot(rownames(A_mat) == rownames(annotations))
 bind_cols(A_mat, annotations[,c("ICA3", "PAMG")]) %>%
   rename(SerDep = ICA3) -> complete_annotation
 
-### Supervised celltype deconvolution
+## RNAseq celltype deconvolution
 mMCPcounter_res <-  read_tsv("../180122_Various/02_Output/mMCPcounter_results.tsv") %>% column_to_rownames("cell_types") %>% t() %>% as_tibble(rownames = "samples")
 ImmuCC_res <-  read_tsv("../180122_Various/02_Output/ImmuCC_results.tsv")
 
+### Choose deconv
+#----------------
 # deconv <- mMCPcounter_res %>% column_to_rownames("samples") %>% .[rownames(A_mat),]
 # decon_title <- "mMCPcounter"
 
 deconv <- ImmuCC_res %>% column_to_rownames("samples") %>% .[rownames(A_mat),]
 decon_title <- "ImmuCC"
+#----------------
 
-
-#### Heatmap
-##### Full (with clustering)
+### Heatmap
 deconv %>% t() %>% pheatmap(main=decon_title, scale = "row", annotation_col = complete_annotation)
 
-#### Corplot
+### Corplot
 stopifnot(rownames(complete_annotation)==rownames(deconv))
 corr_decon <- deconv %>% bind_cols(complete_annotation)
 corr_decon <- corr_decon[rownames(complete_annotation),] # merge mess with the order
@@ -135,44 +134,70 @@ corrplot(corr_decon$r,
          p.mat = corr_decon$P, sig.level = 0.05, insig = "blank")
 
 
-### TF activity
+## TF activity
 tumour_tf <- read_tsv("../180122_Various/02_Output/TFact_tumor_PanCan.tsv")
 stroma_tf <- read_tsv("../180122_Various/02_Output/TFact_stroma_Gtex.tsv")
 
+### Choose tumor/stroma
+#----------------
 tf_activity <- tumour_tf %>% column_to_rownames("TF") %>% dplyr::select(rownames(complete_annotation))
 tf_title <- "tumour"
 
 # tf_activity <- stroma_tf %>% column_to_rownames("TF") %>% dplyr::select(rownames(complete_annotation))
 # tf_title <- "stroma"
+#----------------
 
 mostvar_TF <- Get_mostvar(tf_activity, 50)
-#### Heatmap
-##### Full (with
+### Heatmaps
+
 tf_activity %>%
   pheatmap(main=paste( "TF activity", tf_title), scale = "row", show_rownames = FALSE, annotation_col = complete_annotation)
 
-##### 50 most var
 mostvar_TF %>%
   pheatmap(main=tf_title, scale = "row", annotation_col = complete_annotation)
 
-#### 50 best TFs by component
 PlotBestCorr(complete_annotation, tf_activity, 10, analysis_name = paste(tf_title, "_best_TFs_cyt", sep = ""))
 
 
-### Head and Tail genes
-#### translate to gene names
+# Gene weight analysis
+## translate to gene names
 S_mat %>% as_tibble(rownames = "EnsemblIDs") -> S_gn_
 translate = deframe(annot_ensembl75[c("ensembl_gene_id", "external_gene_id")])
 S_mat %>% rownames() %>% translate[.] -> S_gn_$Genenames
 S_gn_ %>% dplyr::select(!EnsemblIDs) %>%
   relocate(Genenames) -> S_gn
 
-#### load normalized expression data for representation
-genes_toplot_ <- read_tsv("../180122_Various/01_Input/HostCyt_foranalysis.tsv")
-genes_toplot_$EnsemblIDs %>% translate[.] -> genes_toplot_$Genenames
-genes_toplot_ %>% dplyr::select(!EnsemblIDs) %>%
+## load normalized expression data for representation
+genes_toplot_ <- cyt
+genes_toplot_$EnsemblID %>% translate[.] -> genes_toplot_$Genenames
+genes_toplot_ %>% dplyr::select(!EnsemblID) %>%
   relocate(Genenames) -> genes_toplot
 
-#### plot
+## plot
 PlotGeneWeights(S_mat, genes_toplot, 25, complete_annotation, analysis_name = "gene_weights_cyt")
 
+
+## GSVA of best genes
+### gene set preparation
+all_genesets <- msigdbr("Mus musculus")
+all_genesets %>% filter(gs_cat %in% c("H", "C2", "C7")) -> use_genesets
+msigdbr_list = split(x = use_genesets$ensembl_gene, f = use_genesets$gs_name)
+msigdb_descriptions <- use_genesets[c("gs_name", "gs_description")] %>%
+  unique() %>% column_to_rownames("gs_name")
+
+gsvaRes <- gsva(data.matrix(S_mat), msigdbr_list, min.sz = 15)
+
+### Plot
+for (comp in colnames(S_mat)){
+  gsvaRes[order(gsvaRes[,comp]),]
+  
+  gsvaRes %>% as.data.frame() %>%  mutate(the_rank = rank(-get(comp), ties.method = "random")) %>% 
+    dplyr::filter(the_rank < 25 | the_rank > (nrow(gsvaRes)-25)) %>% arrange(the_rank) %>%
+    dplyr::select(!c(the_rank)) -> gsvaTop
+  
+  
+  gsvaTop %>% pheatmap(filename = paste("02_Output/gsva_cyt", comp, ".pdf", sep=""), height = 10 , width = 15,
+                       cluster_rows = F, main = paste(comp, "\n Best gene sets")) # to have a pheatmap
+  # gsvaTop  %>% rownames() %>% msigdb_descriptions[.,] %>% 
+  #   cbind(gsvaTop[comp], .) %>% rownames_to_column("msigdb") %>% write_tsv("02_Output/gsvaRes_IC11nc13.tsv") #to write
+}
