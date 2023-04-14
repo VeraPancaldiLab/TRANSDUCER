1152fc6c047309ee428f94fa690a1277ee36d025
j-solor <jacobo.solorzano@inserm.fr>
2023-04-14 17:26:46 +0200

    first run anota2seq_comparison.R stimulated CAfs 17AC

--- 07_stimulated_CAFs/01_17AC_Conditions/anota2seq_comparison.R
+++ 07_stimulated_CAFs/01_17AC_Conditions/anota2seq_comparison.R
@@ -2,13 +2,12 @@ library(tidyverse)
 library(anota2seq)
 ################################PARAMETERS######################################
 filter_samples = "17AC" # NULL | 17AC | 02136
-exclude_samples = c("Batch_A_17AC_FAKi_Input", "Batch_A_17AC_TGF_F8")
+exclude_samples = NULL # NULL c("Batch_A_17AC_FAKi", "Batch_A_17AC_TGF")
 ################################################################################
 setwd("/home/jacobo/Documents/02_TRANSDUCER/07_stimulated_CAFs/01_17AC_Conditions/")
 
 # Data loading
 counts <- read_tsv("../00_Data/rawcounts.tsv")
-manip_info <- read_tsv("../00_Data/Data_RNA_sample_Jacobo.tsv")
 
 ## filter of samples
 if (!is.null(filter_samples)){
@@ -18,3 +17,36 @@ if (!is.null(filter_samples)){
 if (!is.null(exclude_samples)){
   counts <- dplyr::select(counts, Geneid, names(counts)[!names(counts) %in% exclude_samples])
 }
+
+## Metadata
+manip_info <- read_tsv("../00_Data/Data_RNA_sample_Jacobo.tsv") %>% 
+  dplyr::filter(sample_name %in% names(counts)) %>%
+  dplyr::mutate(sample_name = fct(sample_name, levels=names(counts)[-1])) %>%
+  dplyr::arrange(sample_name)
+
+all(names(counts)[-1] == manip_info$sample_name) %>% stopifnot()
+
+# Anota2seq
+dataP <-  dplyr::select(counts, Geneid, deframe(manip_info[manip_info$Fraction=="F8","sample_name"]))
+dataT <-  dplyr::select(counts, Geneid, deframe(manip_info[manip_info$Fraction=="Input","sample_name"]))
+phenoVec <- dplyr::filter(manip_info, Fraction=="F8") %>% # just to select one of the fractions
+  dplyr::select(Condition) %>% 
+  deframe()
+
+BatchVec <- dplyr::filter(manip_info, Fraction=="F8") %>%
+  dplyr::select(Batch) %>% 
+  deframe()
+
+ads <- anota2seqDataSetFromMatrix(
+  dataP = column_to_rownames(dataP, "Geneid"),
+  dataT = column_to_rownames(dataT, "Geneid"),
+  phenoVec = phenoVec,
+  #batchVec = BatchVec,
+  dataType = "RNAseq",
+  filterZeroGenes = TRUE,
+  normalize = TRUE,
+  transformation = "TMM-log2",
+  varCutOff = NULL)
+
+ads <- anota2seqPerformQC(Anota2seqDataSet = ads,
+                          generateSingleGenePlots = TRUE)
