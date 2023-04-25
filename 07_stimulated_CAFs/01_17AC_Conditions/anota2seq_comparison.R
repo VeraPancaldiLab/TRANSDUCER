e2f5b87d328bd8641609e2e6c058cb9a0a482e74
j-solor <jacobo.solorzano@inserm.fr>
2023-04-25 18:10:00 +0200

    Fixed problem get Regmodes CAF stimuli data in anota2seq_comparison.R
    caused by being too restrictive with thresholds!

--- 07_stimulated_CAFs/01_17AC_Conditions/anota2seq_comparison.R
+++ 07_stimulated_CAFs/01_17AC_Conditions/anota2seq_comparison.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(anota2seq)
+library(biomaRt)
 ################################PARAMETERS######################################
 filter_samples = "17AC" # NULL | 17AC | 02136
 filter_genes = "custom" # custom | allzeros | NULL
@@ -68,3 +69,89 @@ ads <- anota2seqPerformQC(ads,
                           generateSingleGenePlots = TRUE)
 
 ads <- anota2seqResidOutlierTest(ads)
+
+## Analysis
+phenoLev <- unique(phenoVec)
+myContrast <- matrix(nrow =length(phenoLev),ncol=length(phenoLev)-1)
+rownames(myContrast) <- phenoLev
+myContrast[,1] = c(1,0,0,-1,0)
+myContrast[,2]= c(0,1,0,-1,0)
+myContrast[,3]= c(0,0,1,-1,0)
+myContrast[,4]= c(0,0,0,-1,1)
+
+
+ads <- anota2seqAnalyze(ads, contrasts = myContrast,
+                 correctionMethod = "BH", useProgBar = TRUE, fileStem = "ANOTA2SEQ",
+                 analysis = c("translation", "buffering", "translated mRNA", "total mRNA"))
+
+
+#### Check threshold passing in contrast 1 (POSSIBLE ERROR)
+ads@translation@apvStatsRvm[[3]] %>%
+  as_tibble(rownames = "EnsemblID") %>% summary()
+
+ads@buffering@apvStatsRvm[[3]] %>%
+  as_tibble(rownames = "EnsemblID") %>% summary()
+
+ads@translatedmRNA@apvStatsRvm[[3]] %>%
+  as_tibble(rownames = "EnsemblID") %>% summary()
+
+ads@totalmRNA@apvStatsRvm[[3]] %>%
+  as_tibble(rownames = "EnsemblID") %>% summary()
+
+
+ads <- anota2seqSelSigGenes(Anota2seqDataSet = ads,
+                            selContrast = 1,
+                            minSlopeTranslation = -1,
+                            maxSlopeTranslation = 2,
+                            minSlopeBuffering = -2,
+                            maxSlopeBuffering = 1,
+                            maxPAdj = 0.25)
+
+ads <- anota2seqSelSigGenes(Anota2seqDataSet = ads,
+                            selContrast = 2,
+                            minSlopeTranslation = -1,
+                            maxSlopeTranslation = 2,
+                            minSlopeBuffering = -2,
+                            maxSlopeBuffering = 1,
+                            maxPAdj = 1)
+## NEAA vs NT
+ads <- anota2seqSelSigGenes(Anota2seqDataSet = ads,
+                            selContrast = 3,
+                            minSlopeTranslation = -1,
+                            maxSlopeTranslation = 2,
+                            minSlopeBuffering = -2,
+                            maxSlopeBuffering = 1,
+                            maxPAdj = 0.25)
+
+anota2seqGetOutput(object = ads, output="regModes",
+                   selContrast = 3, analysis="translation",
+                   getRVM = TRUE)
+
+anota2seqGetOutput(object = ads, output="regModes",
+                   selContrast = 3, analysis="buffering",
+                   getRVM = TRUE)
+
+anota2seqGetOutput(object = ads, output="regModes",
+                   selContrast = 3, analysis="mRNA abundance",
+                   getRVM = TRUE)
+
+ads <- anota2seqSelSigGenes(Anota2seqDataSet = ads,
+                            selContrast = 4,
+                            minSlopeTranslation = -1,
+                            maxSlopeTranslation = 2,
+                            minSlopeBuffering = -2,
+                            maxSlopeBuffering = 1,
+                            maxPAdj = 0.25)
+anota2seqGetOutput(object = ads, output="regModes",
+                   selContrast = 3, analysis="mRNA abundance",
+                   getRVM = TRUE)
+
+for (i in seq(1,4)){
+  name = paste0(rownames(myContrast)[myContrast[,i]==1], " vs. NT")
+  par(mfrow = c(1, 2))
+  anota2seqPlotPvalues(ads, selContrast = i, useRVM =TRUE, plotToFile = FALSE, contrastName = name)
+
+  ads <- anota2seqRegModes(ads, c(TRUE, TRUE))
+  anota2seqPlotFC(ads, selContrast = i, plotToFile = FALSE, contrastName = name)
+}
+
