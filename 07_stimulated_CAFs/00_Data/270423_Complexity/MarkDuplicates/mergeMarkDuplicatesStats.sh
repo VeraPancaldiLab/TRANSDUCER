e732e109b12925b204fcf85306209713c113de72
j-solor <jacobo.solorzano@inserm.fr>
2023-05-03 15:45:53 +0200

    fix duplicated entry for first file

--- 07_stimulated_CAFs/00_Data/270423_Complexity/MarkDuplicates/mergeMarkDuplicatesStats.sh
+++ 07_stimulated_CAFs/00_Data/270423_Complexity/MarkDuplicates/mergeMarkDuplicatesStats.sh
@@ -5,7 +5,7 @@ do
 
 	while [ $isfirst == 1 ]
 	do
-		awk -v F=$file_clean 'FNR==7{print "FILE\t"$0}; FNR==8{print F"\t"$0}' $file
+		awk -v F=$file_clean 'FNR==7{print "FILE\t"$0}' $file
 		isfirst=0
 	done
 	awk -v F=$file_clean 'FNR==8{print F"\t"$0}' "$file"
