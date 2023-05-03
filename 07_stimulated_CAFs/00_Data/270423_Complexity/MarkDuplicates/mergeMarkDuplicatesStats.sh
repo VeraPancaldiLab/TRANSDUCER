3880cd519b3a7d068d366211c2ef5077c2849e1c
j-solor <jacobo.solorzano@inserm.fr>
2023-05-03 15:26:50 +0200

    adapt file name to match .bam

--- 07_stimulated_CAFs/00_Data/270423_Complexity/MarkDuplicates/mergeMarkDuplicatesStats.sh
+++ 07_stimulated_CAFs/00_Data/270423_Complexity/MarkDuplicates/mergeMarkDuplicatesStats.sh
@@ -1,12 +1,14 @@
 isfirst=1
 for file in metrics_*.bam.txt
 do
+	file_clean=${file#metrics_sorted_}
+
 	while [ $isfirst == 1 ]
 	do
-		awk -v F=$file 'FNR==7{print "File\t"$0}; FNR==8{print F"\t"$0}' $file
+		awk -v F=$file_clean 'FNR==7{print "File\t"$0}; FNR==8{print F"\t"$0}' $file
 		isfirst=0
 	done
-	awk -v F=$file 'FNR==8{print F"\t"$0}' "$file"
+	awk -v F=$file_clean 'FNR==8{print F"\t"$0}' "$file"
 	
 done
 
