190063938a94859ef2f6d7c05e03277d7dec3af5
j-solor <jacobo.solorzano@inserm.fr>
2023-09-16 14:17:28 +0200

    finish analysis IC.6 gene weights Anota2seqUtils (BUG CODON and MOTIF)

--- 02_PDX_stroma/03_Analysis/140923_ICA.TEs_Anota2seqUtils/IC.6.TEs_Anota2seqUtils.R
+++ 02_PDX_stroma/03_Analysis/140923_ICA.TEs_Anota2seqUtils/IC.6.TEs_Anota2seqUtils.R
@@ -31,7 +31,7 @@ annot_ensembl75 <- getBM(attributes = c('ensembl_gene_id',
 gene_to_ensembl = deframe(annot_ensembl75[c( "external_gene_id", "ensembl_gene_id")])
 ensembl_to_gene = setNames(names(gene_to_ensembl), gene_to_ensembl)
 
-#Data loading
+# Data loading
 ## ICs
 ICA_TEs <- read_rds("../100122_ICABoot/02_Output/ICA_TEs.RDS")
 
@@ -47,14 +47,6 @@ TEs_m <- read_tsv("../081221_TranslationEfficacy/02_Output/TEs.tsv")
 annotations <- read_tsv("../../00_Data/Processed_data/sample_info.tsv") %>%
   dplyr::select(sample, PAMG, ICA1, ICA3) %>%
   dplyr::rename(ISRact = ICA3)
-
-# Anota2seqUtils of the Gene weights
-S_TEs <- as_tibble(ICA_TEs$S, rownames = "EnsemblID") %>%
-  dplyr::mutate(geneID = ensembl_to_gene[EnsemblID]) %>%
-  drop_na(geneID) %>% 
-  distinct(geneID, .keep_all = TRUE) %>%
-  dplyr::select(geneID, matches("IC."))
-
 
 # Anota2seqUtils of the TE of IC.6 high samples
 A_TEs <- as_tibble(ICA_TEs$A, rownames = "sample")
@@ -81,7 +73,7 @@ TEs_subset_mean <- dplyr::mutate(TEs_subset, IC.6high = rowMeans(TEs_subset[-1])
   dplyr::select(geneID, IC.6high) %>% 
   dplyr::arrange(abs(IC.6high))
 
-TEs_5perc <- tail(TEs_subset_mean, n = dim(TEs_subset_mean)[1]*5/100)
+TEs_5perc <- tail(TEs_subset_mean, n = 1000)
 TEs_5perc_up <- dplyr::filter(TEs_5perc, IC.6high > 0)
 TEs_5perc_down <- dplyr::filter(TEs_5perc, IC.6high < 0)
   
@@ -93,7 +85,7 @@ library(anota2seqUtils)
 annot <- retrieveFormatData(source = "load", species = "mouse")
 
 ### length
-len <- lengthAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+len_perc <- lengthAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
                       customBg = TEs_subset$geneID, # like so we need to give some background
                       regulation = c("translationUp", "translationDown"),
                       contrast = c(1,1), 
@@ -104,8 +96,8 @@ len <- lengthAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you 
                       pdfName = "results/extremeTEs/5percabs")
 
 
-# Compare difference in nucleotide composition
-content <- contentAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+### Compare difference in nucleotide composition
+content_perc <- contentAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
                            customBg = TEs_subset$geneID, # like so we need to give some background
                            regulation = c("translationUp", "translationDown"),
                            contrast = c(1,1), 
@@ -117,8 +109,8 @@ content <- contentAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object 
                            plotType = 'boxplot',
                            pdfName = "results/extremeTEs/5percabs")
 
-# Compare presence of uORFs
-uorf_strong <- uorf_analysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+### Compare presence of uORFs
+uorf_strong_perc <- uorf_analysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
                              customBg = TEs_subset$geneID, # like so we need to give some background
                              regulation = c("translationUp", "translationDown"),
                              contrast = c(1,1),
@@ -130,7 +122,7 @@ uorf_strong <- uorf_analysis(geneList = TEs_5perc_l, #instead of anota2seq objec
                              unitOut = "number",
                              pdfName = "results/extremeTEs/5percabs")
 
-# # Run de novo sequence analysis
+# ###  Run de novo sequence analysis
 # deNovo <- motifAnalysis(annot = annot,
 #                         memePath = "/home/jacobo/meme/bin/",
 #                         geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
@@ -141,7 +133,7 @@ uorf_strong <- uorf_analysis(geneList = TEs_5perc_l, #instead of anota2seq objec
 #                         subregion = NULL,
 #                         subregionSel=NULL)
 # 
-# # Quantify presence of motifs
+# ###  Quantify presence of motifs
 # motifs <- contentMotifs(annot = annot,
 #                         geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
 #                         customBg = TEs_subset$geneID, # like so we need to give some background
@@ -157,8 +149,8 @@ uorf_strong <- uorf_analysis(geneList = TEs_5perc_l, #instead of anota2seq objec
 #                         pdfName = "results/extremeTEs/5percabs"
 # )
 
-# Folding Energies
-feOut <- foldingEnergyAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+### Folding Energies
+feOut_perc <- foldingEnergyAnalysis(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
                                customBg = TEs_subset$geneID, # like so we need to give some background
                                species = 'mouse',
                                regulation = c("translationUp", "translationDown"),
@@ -170,23 +162,24 @@ feOut <- foldingEnergyAnalysis(geneList = TEs_5perc_l, #instead of anota2seq obj
                                pdfName = "results/extremeTEs/5percabs",
                                residFE = TRUE,
                                plotOut = TRUE)
-# Codon Analysis
-codonOut <- codonUsage(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
-                       customBg = TEs_subset$geneID, # like so we need to give some background
-                       analysis = "codon",
-                       regulation = c("translationUp", "translationDown"),
-                       contrast = c(1,1),
-                       type = "sequence",
-                       comparisons = list(c(1,2)),
-                       annotType = 'ccds',
-                       sourceCod = "load",
-                       selection = "longest",
-                       pAdj=0.01,
-                       plotHeatmap = TRUE,
-                       pdfName = "results/extremeTEs/5percabs",
-                       species = "mouse")
+# ### Codon Analysis (FAIL)
+# codonOut_perc <- codonUsage(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+#                        customBg = TEs_subset$geneID, # like so we need to give some background
+#                        analysis = "codon",
+#                        annot = annot,
+#                        regulation = c("translationUp", "translationDown"),
+#                        contrast = c(1,1),
+#                        type = "sequence",
+#                        comparisons = list(c(1,2)),
+#                        annotType = 'ccds',
+#                        sourceCod = "load",
+#                        selection = "longest",
+#                        pAdj=0.01,
+#                        plotHeatmap = TRUE,
+#                        pdfName = "results/extremeTEs/5percabs",
+#                        species = "mouse")
 
-# selCodonOut <- codonCalc(codonIn = codonOut[['codonAll']], analysis = "codon",
+# ### selCodonOut_perc <- codonCalc(codonIn = codonOut[['codonAll']], analysis = "codon",
 #                          featsel = codonOut[[1]], 
 #                          unit = "freq", 
 #                          regulation = c("translationUp", "translationDown"),
@@ -196,3 +189,187 @@ codonOut <- codonUsage(geneList = TEs_5perc_l, #instead of anota2seq object  you
 #                          pdfName = 'Up',
 #                          plotOut = T,
 #                          plotType = 'ecdf')
+
+
+
+### No human signatures, thus not straightforward testing
+
+## Feature Integration
+features_perc <- c(len_perc,
+              content_perc,
+              uorf_strong_perc,
+              #motifs_perc,
+              feOut_perc #,
+              #selCodonOut, 
+              #sign
+              )
+
+featureIntegration(geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+                   customBg = TEs_subset$geneID, # like so we need to give some background
+                   effectMeasure = deframe(TEs_5perc),
+                   contrastSel = 1,
+                   features = features_perc,
+                   pdfName = "results/extremeTEs/5percabs",
+                   regOnly = T,
+                   allFeat = F,
+                   regulationGen = "translation",
+                   analysis_type = "lm")
+
+################################################################################
+# Anota2seqUtils of the Gene weights
+S_TEs <- as_tibble(ICA_TEs$S, rownames = "EnsemblID") %>%
+  dplyr::mutate(geneID = ensembl_to_gene[EnsemblID]) %>%
+  drop_na(geneID) %>% 
+  distinct(geneID, .keep_all = TRUE) %>%
+  dplyr::select(geneID, matches("IC."))
+
+## selection of high and low genes
+hard_threshold <- if_else(abs(S_TEs$IC.6) > 1, if_else(S_TEs$IC.6 > 0, "high", "low"), NA)
+soft_threshold <- if_else(S_TEs$IC.6 > 0, "high", "low")
+
+threshold = hard_threshold
+S_TEs$threshold = threshold
+
+ggplot(S_TEs, aes(IC.6)) +
+  geom_density() +
+  geom_rug(aes(color = threshold)) +
+  theme_bw()
+
+## data processing
+S_TEs_extup <- dplyr::filter(S_TEs, threshold == "high")
+S_TEs_extdown <- dplyr::filter(S_TEs, threshold == "low")
+
+S_TEs_ext_l <- list(translationUp = S_TEs_extup$geneID,
+                    translationDown = S_TEs_extdown$geneID)
+
+### length
+len_ext <- lengthAnalysis(geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+                      customBg = S_TEs$geneID, # like so we need to give some background
+                      regulation = c("translationUp", "translationDown"),
+                      contrast = c(1,1), 
+                      region = c('UTR5', 'CDS', 'UTR3'),
+                      selection = 'longest', 
+                      annot = annot,
+                      plotType = 'boxplot',
+                      pdfName = "results/extremeIC.6GW/abs1")
+
+
+### Compare difference in nucleotide composition
+content_ext <- contentAnalysis(geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+                           customBg = S_TEs$geneID, # like so we need to give some background
+                           regulation = c("translationUp", "translationDown"),
+                           contrast = c(1,1), 
+                           region = c('UTR5', 'CDS', 'UTR3'),
+                           selection = 'longest', 
+                           annot = annot, 
+                           comparisons = list(c(0,1),c(0,2),c(1,2)),
+                           contentIn = c("G", "C", "A", "T"),
+                           plotType = 'boxplot',
+                           pdfName = "results/extremeIC.6GW/abs1")
+
+### Compare presence of uORFs
+uorf_strong_ext <- uorf_analysis(geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+                             customBg = S_TEs$geneID, # like so we need to give some background
+                             regulation = c("translationUp", "translationDown"),
+                             contrast = c(1,1),
+                             onlyUTR5 = F,
+                             startCodon = "ATG",
+                             KozakContext = "strong",
+                             selection = "longest",
+                             annot = annot,
+                             unitOut = "number",
+                             pdfName = "results/extremeIC.6GW/abs1")
+
+###  Run de novo sequence analysis #BUG?
+# deNovo <- motifAnalysis( geneList = S_TEs_ext_l, 
+#                          customBg = S_TEs$geneID, 
+#                          annot = annot,
+#                          memePath = "/home/jacobo/meme/bin/",
+#                          regulation = c("translationUp", "translationDown"),
+#                          contrast = c(1,1),
+#                          region = c('UTR5', 'CDS', 'UTR3'),
+#                          subregion = NULL,
+#                          subregionSel=NULL)
+#
+# ###  Quantify presence of motifs #BUG?
+# motifs <- contentMotifs(annot = annot,
+#                         geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+#                         customBg = S_TEs$geneID, # like so we need to give some background
+#                         regulation = c("translationUp", "translationDown"),
+#                         contrast = c(1,1),
+#                         motifsIn = deNovo[[1]]$motifsOut,
+#                         region = "UTR3",
+#                         dist = 1,
+#                         selection = "longest",
+#                         annot = annot,
+#                         comparisons = list(c(1,2)),
+#                         unitOut = "number",
+#                         pdfName = "results/extremeIC.6GW/abs1"
+# )
+
+### Folding Energies
+feOut_ext <- foldingEnergyAnalysis(geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+                                    customBg = S_TEs$geneID, # like so we need to give some background
+                                    species = 'mouse',
+                                    regulation = c("translationUp", "translationDown"),
+                                    contrast = c(1,1), 
+                                    region = c('UTR5', 'CDS', 'UTR3'),
+                                    selection = 'longuest', 
+                                    annot = annot,
+                                    plotType = 'ecdf',
+                                    pdfName = "results/extremeIC.6GW/abs1",
+                                    residFE = TRUE,
+                                    plotOut = TRUE)
+
+# ### Codon Analysis (FAIL)
+# codonOut_perc <- codonUsage(geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+#                             customBg = S_TEs$geneID, # like so we need to give some background
+#                             analysis = "codon",
+#                             annot = annot,
+#                             regulation = c("translationUp", "translationDown"),
+#                             contrast = c(1,1),
+#                             type = "sequence",
+#                             comparisons = list(c(1,2)),
+#                             annotType = 'ccds',
+#                             sourceCod = "load",
+#                             selection = "longest",
+#                             pAdj=0.01,
+#                             plotHeatmap = TRUE,
+#                             pdfName = "results/extremeIC.6GW/abs1",
+#                             species = "mouse")
+
+# ### selCodonOut_perc <- codonCalc(codonIn = codonOut[['codonAll']], analysis = "codon",
+#                          featsel = codonOut[[1]], 
+#                          unit = "freq", 
+#                          regulation = c("translationUp", "translationDown"),
+#                          contrast = c(1,1),
+#                          geneList = TEs_5perc_l, #instead of anota2seq object  you input geneList and effect_measures
+#                          customBg = TEs_subset$geneID, # like so we need to give some background
+#                          pdfName = 'Up',
+#                          plotOut = T,
+#                          plotType = 'ecdf')
+
+
+
+### No human signatures, thus not straightforward testing
+
+## Feature Integration
+features_ext <- c(len_ext,
+                   content_ext,
+                   uorf_strong_ext,
+                   #motifs_perc,
+                   feOut_ext #,
+                   #selCodonOut, 
+                   #sign
+)
+
+featureIntegration(geneList = S_TEs_ext_l, #instead of anota2seq object  you input geneList and effect_measures
+                   customBg = S_TEs$geneID, # like so we need to give some background
+                   effectMeasure = dplyr::select(S_TEs,geneID, IC.6) %>% deframe(),
+                   contrastSel = 1,
+                   features = features_ext,
+                   pdfName = "results/extremeIC.6GW/abs1",
+                   regOnly = T,
+                   allFeat = F,
+                   regulationGen = "translation",
+                   analysis_type = "lm")
