cad76f3df5abfbcc1752b578577936a023c6d79a
j-solor <jacobo.solorzano@inserm.fr>
2023-09-25 14:51:28 +0200

    fix MID Network for ICA_TEs analysis

--- 02_PDX_stroma/03_Analysis/100122_ICABoot/ICA_TEs.R
+++ 02_PDX_stroma/03_Analysis/100122_ICABoot/ICA_TEs.R
@@ -316,16 +316,16 @@ system(paste(c("cd 02_Output/ \n convert", concat_pdf, "ICA_TEs.pdf"), collapse 
 # Network analysis
 MID <- read_csv("01_Input/MID2022.csv")
 
-trans_mgi_ensembl <- deframe(annot_ensembl75[c("mgi_id", "ensembl_gene_id")])
+trans_mgi_name <- deframe(annot_ensembl75[c("mgi_id", "external_gene_id")])
 trans_ensembl_name <- deframe(annot_ensembl75[c("ensembl_gene_id", "external_gene_id")])
 
-nodes <- as_tibble(S_mat, rownames = "id") %>%
-  mutate(gene_name = trans_ensembl_name[id]) %>%
-  relocate(gene_name, .after = "id")
+nodes <- as_tibble(S_mat, rownames = "ensembl_id") %>%
+  mutate(id = trans_ensembl_name[ensembl_id]) %>%
+  relocate(ensembl_id, .after = "id")
 
 edges <- mutate(MID,
-                gene1 = trans_mgi_ensembl[gene1],
-                gene2 = trans_mgi_ensembl[gene2]) %>%
+                gene1 = trans_mgi_name[gene1],
+                gene2 = trans_mgi_name[gene2]) %>%
   dplyr::filter(gene1 %in% nodes$id, 
                 gene2 %in% nodes$id) %>%
   dplyr::rename(source = gene1,
@@ -333,7 +333,7 @@ edges <- mutate(MID,
                 interaction = type) %>%
   dplyr::select(source, target, interaction)
 
-PlotNetwork(nodes, edges, S_mat,valid_comp = 1:6, main_name = "MID_ICATE")
+PlotNetwork(nodes, edges, S_mat,valid_comp = 1:6, main_name = "MID_ICAcyt")
 
 #-------------------------------------------------------------------------------
 
