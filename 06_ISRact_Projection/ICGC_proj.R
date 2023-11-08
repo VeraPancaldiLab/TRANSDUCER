694ec5d1b8450b1e265b0e549e909bf71da0066f
j-solor <jacobo.solorzano@inserm.fr>
2023-11-08 14:13:26 +0100

    add ICGC_proj.R data loading

---
+++ 06_ISRact_Projection/ICGC_proj.R
@@ -0,0 +1,48 @@
+#Import library
+library(tidyverse)
+library(readxl)
+library(biomaRt)
+library(pdacmolgrad) #devtools::install_github("RemyNicolle/pdacmolgrad")
+library(edgeR)
+library(Hmisc)
+library(ggrepel)
+library(DEqMS)
+library(matrixStats)
+library(msigdbr)
+library(fgsea)
+library(ggpubr)
+library(data.table)
+library(ggplot2)
+library(survival)
+library(survminer)
+
+
+################################################################################
+setwd("~/Documents/02_TRANSDUCER/06_ISRact_Projection/")
+source("src/human_cohort_data_filter.R")
+source("src/correlation_plotter.R")
+
+# Data loading
+## Data
+ICGC <- read_rds("data/ICGC/ICGC_AU.rds")
+  
+### Metadata
+ICGC_clinical <- read_rds("data/ICGC/ICGC_AU_survival.rds")
+
+### inherited sample info and top samples from Sauyeun_PDX
+sample_info <- read_delim("data/Sauyeun_PDX/sample_info.tsv", 
+                          delim = "\t", escape_double = FALSE, 
+                          trim_ws = TRUE)
+
+top_samples <- arrange(sample_info, ICA3) %>%
+  dplyr::slice( unique(c(1:5, n() - 0:4)) ) %>%
+  mutate(ISRact = ifelse(ICA3 < 0, "low_ICA3", "high_ICA3")) %>%
+  arrange(sample)
+
+# Import the projection
+## ISRactPCA
+pca_pdx <- read_rds("data/Classifiers/pca_pdx_ENZO.RDS")
+
+################################################################################
+# PARAMETERS
+################################################################################
