a105b4b6105296c27d34df1ec07bd366983f6264
j-solor <jacobo.solorzano@inserm.fr>
2023-12-04 15:29:10 +0100

    failed at generating own normalization due to lack of RAM

--- 06_ISRact_Projection/Hwang_scProj.R
+++ 06_ISRact_Projection/Hwang_scProj.R
@@ -13,9 +13,7 @@ expression_matrix <- ReadMtx(
 )
 
 metadata <- read_tsv("data/Hwang_Nature_2022/GEO/metadata.tsv")  %>% column_to_rownames("...1") 
-
 seurat_object <- CreateSeuratObject(counts = expression_matrix)
-
 seurat_object <- AddMetaData(seurat_object, metadata = metadata)
 
 ## clean unused data
@@ -34,6 +32,30 @@ UMAP_Coord <- read_tsv("data/Hwang_Nature_2022/GEO/obsm/X_umap.tsv", col_types =
 UMAP_coordinates_mat <- as(UMAP_Coord, "matrix")
 
 seurat_object[['UMAP']] <- CreateDimReducObject(embeddings = UMAP_coordinates_mat, key = "UMAP_", global = T, assay = "RNA")
+
+## Create my own UMAP
+# ### Normalise (log2(TP10K+1)) DOUBTS!
+# seurat_object <- NormalizeData(seurat_object,  normalization.method = "LogNormalize", scale.factor = 10000, verbose = T)
+# 
+# ### Select and scale high variable genes (2K) DOUBTS!
+# seurat_object <- FindVariableFeatures(seurat_object, selection.method = "dispersion", nfeatures = 2000)
+# 
+# top10 <- head(VariableFeatures(seurat_object), 10)
+# plot1 <- VariableFeaturePlot(seurat_object)
+# plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+# plot1 + plot2
+# 
+# ### Scale data (not mentioned methods)
+# all.genes <- rownames(seurat_object)
+# seurat_object <- ScaleData(seurat_object, features = all.genes)
+# 
+# ### PCA and num dim exploration FAILED FRO MEMORY ISSUE
+# seurat_object <- RunPCA(seurat_object, features = VariableFeatures(object = seurat_object), verbose = T)
+# DimHeatmap(seurat_object, dims = 1:15, cells = 500, balanced = TRUE)
+# ElbowPlot(seurat_object)
+# 
+# 
+# seurat_object <- RunUMAP(seurat_object, dims = 1:10)
 
 ## Celltype plot
 DimPlot(seurat_object, reduction = "UMAP", group.by ="new_celltypes")
