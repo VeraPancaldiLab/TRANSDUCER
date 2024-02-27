76e8a89b8ad583357d539e78cc10e491e47bdeb0
j-solor <jacobo.solorzano@inserm.fr>
2024-02-27 11:46:07 +0100

    add scRNAseq ISRactPCA high vs low correlation

--- 06_ISRact_Projection/src/scatterplot_with_stats.R
+++ 06_ISRact_Projection/src/scatterplot_with_stats.R
@@ -2,7 +2,7 @@
 # this function calculates "type" statistic and produces a scatterplot 
 # with this stat
 
-scatterplot_with_stats <- function(data, varx, vary, label, type, title){
+scatterplot_with_stats <- function(data, varx, vary, label = FALSE, type, title){
   correlation <- rcorr(data[[varx]], data[[vary]], type = type)
   correlation_txt <- paste0(type, ": R = ", round(correlation$r["x","y"], 2),
                             ", pval = ", round(correlation$P["x","y"], 4))
