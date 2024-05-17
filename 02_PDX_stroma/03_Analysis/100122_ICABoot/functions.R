b41754e35167778053cc15eb9e70049192d2949a
j-solor <jacobo.solorzano@inserm.fr>
2024-05-17 14:27:00 +0200

    add FigR2B and new function

--- 02_PDX_stroma/03_Analysis/100122_ICABoot/functions.R
+++ 02_PDX_stroma/03_Analysis/100122_ICABoot/functions.R
@@ -517,3 +517,19 @@ PlotNetwork <- function(nodes, edges, S_mat, valid_comp, main_name){
 }
 
 
+# From a tibble data, with 2 variables varx adn vary, = an extra label, 
+# this function calculates "type" statistic and produces a scatterplot 
+# with this stat
+
+scatterplot_with_stats <- function(data, varx, vary, label = FALSE, type, title){
+  correlation <- rcorr(data[[varx]], data[[vary]], type = type)
+  correlation_txt <- paste0(type, ": R = ", round(correlation$r["x","y"], 2),
+                            ", pval = ", round(correlation$P["x","y"], 4))
+  ggplot(data) +
+    aes_string(x = varx, y = vary, label = label) +
+    geom_point() +
+    geom_smooth(method=lm) +
+    theme_bw() +
+    geom_text_repel() +
+    labs(title=title, subtitle = correlation_txt)
+}
