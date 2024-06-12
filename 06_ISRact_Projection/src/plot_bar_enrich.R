1b13a9511750be9b5ba24b94c2663ed98f822fe5
j-solor <jacobo.solorzano@inserm.fr>
2024-06-12 12:02:23 +0200

    Mod for FigR27 and FigR28

--- 06_ISRact_Projection/src/plot_bar_enrich.R
+++ 06_ISRact_Projection/src/plot_bar_enrich.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(scales)
 ## Alternative plotting of GO resulting of enrichPathway()
 ## as default Cluisterprofiler fuction do not work
 convert_to_numeric <- function(x) {
@@ -13,7 +14,7 @@ plot_bar_enrich <- function(x, p.adjust_th, Title) {
     slice_tail(n=20) %>%
     ggplot(aes(x = GeneRatioNum, y = Description, fill = p.adjust)) +
     geom_bar(stat="identity") +
-    scale_fill_gradient(low = "red", high = "blue", guide=guide_colourbar(reverse = TRUE))+
+    scale_fill_gradient(low = "red", high = "blue", guide=guide_colourbar(reverse = TRUE), limits = c(10^-11,0.1), trans = "log", breaks = c( 0.05, 0.01, 0.001, 10^-5, 10^-10),oob=squish)+
     theme_bw() +
     labs(title = Title) +
     xlab("Gene Ratio") +
