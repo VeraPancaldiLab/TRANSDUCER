160f046e43bd0082fdfaf77cb427b640ec63c40a
j-solor <jacobo.solorzano@inserm.fr>
2024-08-20 13:44:16 +0200

    adapt and style Maurer Projection

--- 06_ISRact_Projection/Maurer_proj.R
+++ 06_ISRact_Projection/Maurer_proj.R
@@ -1,7 +1,16 @@
-#Import library
+# ISRactPCA projection into Maurer 2019 LMD cohort
+################################################################################
+#' Import different files
+#' Filter data and project ISRactPCA
+#' Produce projection scatterplot
+#' Compare ISRactPCA with PAMG score
+#' For more information run 
+################################################################################
+
+# Import libraries
 library(tidyverse)
 library(biomaRt)
-library(pdacmolgrad) #devtools::install_github("RemyNicolle/pdacmolgrad")
+library(pdacmolgrad) # devtools::install_github("RemyNicolle/pdacmolgrad")
 library(edgeR)
 library(Hmisc)
 library(ggrepel)
@@ -11,75 +20,79 @@ source("src/human_cohort_data_filter.R")
 source("src/correlation_plotter.R")
 
 # Import datasets
-## PACAOMICS
-### Remy Nicolle's 2017 PDX data
+## CPTAC (Proteogenomics)
+### Load data
 load("data/Maurer2019/Start Maurer EvsSvsB.RData")
 
-### inherited sample info and top samples from Sauyeun_PDX
-sample_info <- read_delim("data/Sauyeun_PDX/sample_info.tsv", 
-                          delim = "\t", escape_double = FALSE, 
-                          trim_ws = TRUE)
-
-top_samples <-arrange(sample_info, ICA3) %>%
-  dplyr::slice( unique(c(1:5, n() - 0:4)) ) %>%
-  mutate(ISRact = ifelse(ICA3 < 0, "low_ICA3", "high_ICA3")) %>%
-  arrange(sample)
-
-# Import the projection
-## ISRactPCA
+## ISRactPCA projection
 pca_pdx <- read_rds("data/Classifiers/pca_pdx_ENZO.RDS")
+### inherited sample info and top samples from Shin PDX training data
+sample_info <- read_delim("data/Sauyeun_PDX/sample_info.tsv",
+  delim = "\t", escape_double = FALSE,
+  trim_ws = TRUE
+)
+
+top_samples <- arrange(sample_info, ICA3) %>%
+  dplyr::slice(unique(c(1:5, n() - 0:4))) %>%
+  mutate(ISRact_bin = ifelse(ICA3 < 0, "low_ISRact", "high_ISRact")) %>%
+  dplyr::rename(ISRact = "ICA3") %>%
+  arrange(sample)
 
 ################################################################################
 # PARAMETERS
-norm_method = "upperquartile" #TMM | upperquartile
-subset_celltype = "epithelium" # epithelium | stroma | bulk
+norm_method <- "upperquartile" # TMM | upperquartile
+subset_celltype <- "epithelium" # epithelium | stroma | bulk
 ################################################################################
 
 # Translate EnsemblID to gene names
-## Version 75 for PDX data
-ensembl75 <- useEnsembl(biomart = "genes",
-                        dataset = "hsapiens_gene_ensembl",
-                        version = 109,
-                        host="https://grch37.ensembl.org")#listAttributes(ensembl75, page="feature_page")
+## Version 109
+ensembl75 <- useEnsembl(
+  biomart = "genes",
+  dataset = "hsapiens_gene_ensembl",
+  version = 109,
+  host = "https://grch37.ensembl.org"
+) # listAttributes(ensembl75, page="feature_page")
 
-annot_ensembl75 <- getBM(attributes = c('ensembl_gene_id',
-                                        'hgnc_symbol'), mart = ensembl75)
+annot_ensembl75 <- getBM(attributes = c(
+  "ensembl_gene_id",
+  "hgnc_symbol"
+), mart = ensembl75)
 
-translate = deframe(annot_ensembl75[c("ensembl_gene_id", "hgnc_symbol")])
-inverse_translate = deframe(annot_ensembl75[c( "hgnc_symbol", "ensembl_gene_id")])
+translate <- deframe(annot_ensembl75[c("ensembl_gene_id", "hgnc_symbol")])
+inverse_translate <- deframe(annot_ensembl75[c("hgnc_symbol", "ensembl_gene_id")])
 
 # Preprocessing
 ## Subset cell type in Maurer dataset
 sample_type <- dplyr::filter(phenoEvsS, Compartment == subset_celltype)
 maurer_rawcount <- dplyr::select(raw_E_Vs_S, sample_type$Sample) %>%
-  dplyr::mutate(EnsemblID = inverse_translate[rownames(.)]) %>% 
+  dplyr::mutate(EnsemblID = inverse_translate[rownames(.)]) %>%
   dplyr::group_by(EnsemblID) %>%
   dplyr::summarise_all(sum) %>%
-  dplyr::filter(!is.na(EnsemblID)) %>% 
+  dplyr::filter(!is.na(EnsemblID)) %>%
   column_to_rownames("EnsemblID")
 
 
 maurer_normcount_ <- DGEList(maurer_rawcount) %>%
-  calcNormFactors(method= norm_method) %>%
-  cpm(log=TRUE)
+  calcNormFactors(method = norm_method) %>%
+  cpm(log = TRUE)
 
-maurer_normcount<- t(maurer_normcount_) %>%
+maurer_normcount <- t(maurer_normcount_) %>%
   as_tibble(rownames = "sample")
 
 
 ### Create sample_info_maurer
-type_pamg <- projectMolGrad(newexp = maurer_normcount_,  geneSymbols = translate[rownames(maurer_normcount_)]) %>%
+type_pamg <- projectMolGrad(newexp = maurer_normcount_, geneSymbols = translate[rownames(maurer_normcount_)]) %>%
   as_tibble(rownames = "sample")
 
-maurer_info <- dplyr::select(type_pamg, sample, ICGCrnaseq) %>% 
+maurer_info <- dplyr::select(type_pamg, sample, ICGCrnaseq) %>%
   dplyr::rename(PAMG = ICGCrnaseq)
 
 # Projection
 ## Maurer
 ### Add missing genes to expression before the PCA
-filter_pca <- function(.data, objective){
-  as_tibble(.data, rownames = "tmp") %>% 
-    dplyr::filter(tmp %in% objective) %>% 
+filter_pca <- function(.data, objective) {
+  as_tibble(.data, rownames = "tmp") %>%
+    dplyr::filter(tmp %in% objective) %>%
     column_to_rownames("tmp") %>%
     data.matrix()
 }
@@ -88,38 +101,50 @@ pca_pdx$rotation <- filter_pca(pca_pdx$rotation, names(maurer_normcount)[-1])
 pca_pdx$center <- filter_pca(pca_pdx$center, names(maurer_normcount)[-1])
 pca_pdx$scale <- filter_pca(pca_pdx$scale, names(maurer_normcount)[-1])
 
-projection_maurer <- predict(pca_pdx, column_to_rownames(maurer_normcount,"sample")) %>% 
+Maurer_PCAspace <- predict(pca_pdx, column_to_rownames(maurer_normcount, "sample")) %>%
   as_tibble(rownames = "sample") %>%
-  left_join(maurer_info[,c("sample", "PAMG")], by="sample")
+  left_join(maurer_info[, c("sample", "PAMG")], by = "sample") %>%
+  dplyr::rename(ISRactPCA = "PC1") %>%
+  mutate(
+    ISRactPCA_bin = if_else(ISRactPCA < quantile(ISRactPCA, probs = 0.3333), "low_ISRactPCA",
+      if_else(ISRactPCA < quantile(ISRactPCA, probs = 0.6666), "intermediate_ISRactPCA", "high_ISRactPCA")
+    ),
+    ISRactPCA_bin = fct(ISRactPCA_bin, levels = c("low_ISRactPCA", "intermediate_ISRactPCA", "high_ISRactPCA"))
+  )
 
-maurer_PC1 <- arrange(projection_maurer, PC1) %>% 
-  mutate(PC1status = cut(.$PC1, breaks = c(quantile(.$PC1, c(0:3/3))), labels = c("low_PC1", "medium_PC1", "high_PC1"), include.lowest = TRUE)) %>%
-  dplyr::select(sample, PC1,  PC1status)
+Maurer_ISRact_projection <- arrange(Maurer_PCAspace, ISRactPCA) %>%
+  dplyr::select(sample, ISRactPCA, ISRactPCA_bin)
 
 
 # Plot pca and projections
 ## Add ISR status to PCA df
-pca_full_df <- pca_pdx[["x"]] %>%
+pca_training <- pca_pdx[["x"]] %>%
   as.data.frame() %>%
   rownames_to_column("sample") %>%
-  inner_join(top_samples[,c("sample","ISRact")])
+  dplyr::rename(ISRactPCA = "PC1") %>%
+  inner_join(top_samples[, c("sample", "ISRact_bin")])
 
 #-------------------------------------------------------------------------------
 # Plot projecion
 
-show_projection <- bind_rows(as_tibble(pca_full_df) %>% mutate(dataset = "Sauyeun PDX"),
-                             as_tibble(projection_maurer %>% mutate(ISRact =  "Unknown",
-                                                                  dataset = "Maurer")))
+show_projection <- bind_rows(
+  as_tibble(pca_training) %>% mutate(dataset = "Sauyeun PDX"),
+  as_tibble(Maurer_PCAspace %>% mutate(
+    ISRact_bin = "Unknown",
+    dataset = "Maurer"
+  ))
+)
 
-dplyr::mutate(show_projection, ISRact = str_replace(ISRact, 'ICA3', 'ISRact')) %>%
-  dplyr::filter(dataset %in% c("Sauyeun PDX","PACAOMICS PDX", "CPTAC", "CCLE", "Maurer"), # "Sauyeun PDX","PACAOMICS PDX", "CPTAC", "CCLE"
-                ISRact %in% c('low_ISRact', 'high_ISRact', 'medium_ISRact', 'Unknown')) %>% 
-  ggplot(aes(x=PC1, y=PC2, color = ISRact, shape = dataset)) +
+dplyr::filter(
+  show_projection, dataset %in% c("Sauyeun PDX", "PACAOMICS PDX", "CPTAC", "CCLE", "Maurer"), # "Sauyeun PDX","PACAOMICS PDX", "CPTAC", "CCLE"
+  ISRact_bin %in% c("low_ISRact", "high_ISRact", "intermediate_ISRact", "Unknown")
+) %>%
+  ggplot(aes(x = ISRactPCA, y = PC2, color = ISRact_bin, shape = dataset)) +
   geom_point() +
   scale_shape_discrete(limits = c("Sauyeun PDX", "PACAOMICS PDX", "CPTAC", "CCLE", "Maurer")) +
-  scale_color_discrete(limits = c('low_ISRact', 'high_ISRact', 'medium_ISRact', 'Unknown')) #c('low_ISRact', 'high_ISRact', 'medium_ISRact', 'Unknown'))unique(projection_ccle$primary_tissue)
+  scale_color_discrete(limits = c("low_ISRact", "high_ISRact", "intermediate_ISRact", "Unknown")) # c('low_ISRact', 'high_ISRact', 'intermediate_ISRact', 'Unknown'))unique(projection_ccle$primary_tissue)
 #-------------------------------------------------------------------------------
 
 # Plot comparisons with Basal/Classical and ISRact
-## PAMG vs PC1
-correlation_plotter(data = projection_maurer, col1 = "PAMG", col2 = "PC1", data_name = "Maurer")
+## PAMG vs ISRactPCA
+correlation_plotter(data = Maurer_PCAspace, col1 = "PAMG", col2 = "ISRactPCA", data_name = "Maurer")
---
+++ 06_ISRact_Projection/Maurer_proj_testgenesj.R
@@ -0,0 +1,381 @@
+# DGEA on Maurer cohort RNA-seq rawcount #! Need to adapt PC1 -> ISRactPCA
+
+################################################################################
+#' Import rawcount
+#' follow the limma voom workflow for DGEA analysis
+#' GSEA on ranking regarding DGEA results
+################################################################################
+
+library(readr)
+library(readxl)
+library(edgeR)
+library(limma)
+library(ggplot2)
+library(ggpubr)
+library(tidyverse)
+library(FactoMineR)
+library(factoextra)
+library(EnhancedVolcano)
+library(fgsea)
+library(msigdbr)
+library(RColorBrewer)
+library(dorothea)
+library(pheatmap)
+
+
+## setwd and import local functions
+setwd("~/Documents/02_TRANSDUCER/06_ISRact_Projection/")
+source("src/plot_PCs.R")
+source("src/plotBestPathways.R")
+
+################################################################################
+subset_celltype <- "bulk" # epithelium | stroma | bulk
+################################################################################
+# Import data
+## Expression
+load("data/Maurer2019/Start Maurer EvsSvsB.RData")
+
+## Metadata
+### Import ISRactPCS projection of different subset  #! This comes from 02_TRANSDUCER/06_Enzos_Internship/human_cohort_analysis/Data
+if (subset_celltype == "epithelium") {
+  Maurer_PC1 <- read_csv("results/Maurer/Maurer_epithelium_PC1.csv")
+} else if (subset_celltype == "stroma") {
+  Maurer_PC1 <- read_csv("results/Maurer/Maurer_stroma_PC1.csv")
+} else if (subset_celltype == "bulk") {
+  Maurer_PC1 <- read_csv("results/Maurer/Maurer_bulk_PC1.csv")
+}
+
+### load sample information for annotations
+sample_info <- rename(Maurer_PC1, Sample = sample) %>%
+  inner_join(phenoEvsS, by = "Sample")
+
+# DGEA following the Limma Voom pipeline from https://ucdavis-bioinformatics-training.github.io/2018-June-RNA-Seq-Workshop/thursday/DE.html
+################################################################################
+## Select samples corresponding to the subset cell type
+counts <- dplyr::select(raw_E_Vs_S, Maurer_PC1$sample)
+
+### Visualize raw counts
+random10 <- sort(sample.int(dim(sample_info)[1], 10))
+
+data <- rownames_to_column(counts[, random10], "Prot") %>%
+  pivot_longer(cols = 2:11, names_to = "case_id", values_to = "Expected_count") # pivot_longer data to make them easily plotable with ggplot 2
+
+ggplot(data, mapping = aes(x = case_id, y = Expected_count, fill = case_id)) +
+  geom_violin() +
+  theme(legend.position = "none") +
+  coord_flip() +
+  yscale("log10")
+
+# Follow the Limma Voom pipeline from https://ucdavis-bioinformatics-training.github.io/2018-June-RNA-Seq-Workshop/thursday/DE.html
+
+### Create DGEList object
+d0 <- dplyr::select(counts, sample_info$Sample) %>%
+  DGEList()
+
+### Calculate normalization factors
+d0 <- calcNormFactors(d0)
+d0
+
+### Filter low-expressed genes
+cutoff <- 1
+drop <- which(apply(cpm(d0), 1, max) < cutoff)
+d <- d0[-drop, ]
+dim(d) # number of genes left
+
+### Create a new variable “group”
+group <- as.factor(Maurer_PC1$PC1status)
+
+### Specify the model to be fitted
+mm <- model.matrix(~ 0 + group)
+
+### Filter with filter by expression
+keep <- filterByExpr(d0, mm)
+d <- d0[keep, ]
+dim(d) # number of genes left
+
+### Visualize after normalization and filtering
+dnorm <- cpm(d)
+
+data2 <- as.data.frame(dnorm[, random10]) %>%
+  rownames_to_column("Prot") %>%
+  pivot_longer(cols = 2:11, names_to = "case_id", values_to = "count_norm") # pivot_longer data to make them easily plotable with ggplot 2
+
+ggplot(data2, mapping = aes(x = case_id, y = count_norm, fill = case_id)) +
+  geom_violin() +
+  theme(legend.position = "none") +
+  coord_flip() +
+  yscale("log10")
+
+### Multidimensional scaling (MDS) plot
+plotMDS(d, col = as.numeric(group))
+
+### PCA plot regarding clinical and phenotype data
+
+dnorm.t <- t(dnorm)
+dnorm.pca <- PCA(dnorm.t, ncp = 10, graph = FALSE)
+summary(dnorm.pca)
+
+fviz_eig(dnorm.pca, addlabels = TRUE, ylim = c(0, 50))
+eig.val <- get_eigenvalue(dnorm.pca)
+eig.val # 63 dimensions to reach 90% of explained variance
+
+# Join PCA results and more relevant clinical data
+pca_fulldf <- dnorm.pca$ind$coord %>%
+  as_tibble(rownames = "Sample")
+
+for (feat in colnames(pca_fulldf)[-c(1:11)]) {
+  plot_PCs(pca_fulldf, feat, 10, 1)
+}
+
+## Voom transformation and calculation of variance weights
+y <- voom(d, mm, plot = T)
+
+## Fitting linear models in limma
+fit <- lmFit(y, mm)
+head(coef(fit))
+
+### define contrast matrix
+contr <- makeContrasts(grouphigh_PC1 - grouplow_PC1, levels = colnames(coef(fit)))
+contr
+
+tmp <- contrasts.fit(fit, contr) # Estimate contrast for each gene
+
+tmp <- eBayes(tmp) # Empirical Bayes smoothing of standard errors
+
+## Visualize top differentiated genes with vulcano plot
+top_diff_gene <- topTreat(tmp, coef = 1, n = Inf) %>%
+  rownames_to_column("external_gene_name")
+EnhancedVolcano(top_diff_gene,
+  lab = top_diff_gene$external_gene_name, x = "logFC", y = "P.Value",
+  pCutoffCol = "adj.P.Val", pCutoff = 0.05, title = "Low PC1 vs. high PC1",
+  subtitle = "adj.P.Val < 0.05"
+)
+
+
+### Other visualisations of the vignettes
+top.table <- topTable(tmp, sort.by = "P", n = Inf) %>%
+  mutate(
+    Expression = case_when(
+      logFC >= log(2) & adj.P.Val <= 0.05 ~ "Up-regulated",
+      logFC <= -log(2) & adj.P.Val <= 0.05 ~ "Down-regulated",
+      TRUE ~ "Unchanged"
+    )
+  ) %>%
+  rownames_to_column("Gene")
+
+head(top.table, 20)
+
+# How many DE genes are there?
+length(which(top.table$adj.P.Val < 0.05))
+
+dt <- decideTests(fit)
+summary(dt)
+vennDiagram(dt[, 1:2], circle.col = c("turquoise", "salmon"))
+
+# GSEA on l2FC
+## load gene sets
+msigdbr_reactome_df <- msigdbr(species = "Homo sapiens", category = "C2", subcategory = "CP:REACTOME")
+msigdbr_hallmark_df <- msigdbr(species = "Homo sapiens", category = "H")
+
+## extract ranked list
+ranked_genes <- top_diff_gene %>%
+  as_tibble() %>%
+  arrange(desc(logFC))
+ranked_genes <- ranked_genes[, c(1, 2)] %>% deframe()
+
+msigdbr_list_reactome <- split(x = msigdbr_reactome_df$gene_symbol, f = msigdbr_reactome_df$gs_name)
+msigdbr_list_hallmark <- split(x = msigdbr_hallmark_df$gene_symbol, f = msigdbr_hallmark_df$gs_name)
+
+## analysis
+gsea_res_reactome <- fgsea(pathways = msigdbr_list_reactome, stats = ranked_genes, minSize = 15)
+gsea_res_hallmark <- fgsea(pathways = msigdbr_list_hallmark, stats = ranked_genes, minSize = 15)
+
+
+## Plots
+### Official viggnette plots
+plotBestPathways(gsea_res_reactome, msigdbr_list_reactome, ranked_genes)
+plotBestPathways(gsea_res_hallmark, msigdbr_list_hallmark, ranked_genes)
+
+### custom plots thesis
+#### Reactome
+topPathwaysUp <- gsea_res_reactome[ES > 0][head(order(pval), n = 10), pathway]
+topPathwaysDown <- gsea_res_reactome[ES < 0][head(order(pval), n = 10), pathway]
+topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
+
+maurerbarplot_DEGEA_reactome <- dplyr::filter(gsea_res_reactome, pathway %in% topPathways) %>%
+  arrange(NES) %>%
+  mutate(
+    pathway = str_remove(pathway, "REACTOME_"),
+    pathway = as_factor(pathway)
+  ) %>%
+  # dplyr::filter(p.adjust < p.adjust_th) %>%
+  # slice_tail(n=20) %>%
+  ggplot(aes(x = NES, y = pathway, fill = padj)) +
+  geom_bar(stat = "identity") +
+  scale_fill_gradientn(
+    colours = c("red", "white", "grey"), guide = guide_colourbar(reverse = TRUE),
+    rescaler = ~ scales::rescale_mid(., mid = 0.05)
+  ) +
+  theme_bw() +
+  labs(title = "REACTOME") +
+  ylab("")
+
+print(maurerbarplot_DEGEA_reactome)
+# ggsave(maurerbarplot_DEGEA_reactome,
+#        filename = paste0("results/Figures/maurer",subset_celltype ,"barplot_DEGEA_reactome.svg"),
+#        width = 8,
+#        height = 5)
+
+#### Hallmark
+topPathwaysUp <- gsea_res_hallmark[ES > 0][head(order(pval), n = 10), pathway]
+topPathwaysDown <- gsea_res_hallmark[ES < 0][head(order(pval), n = 10), pathway]
+topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
+
+maurerbarplot_DEGEA_hallmark <- dplyr::filter(gsea_res_hallmark, pathway %in% topPathways) %>%
+  arrange(NES) %>%
+  mutate(
+    pathway = as_factor(pathway),
+    padj = ifelse(padj < 0.1, padj, NA)
+  ) %>% # else the colour bar is screwed
+  # dplyr::filter(p.adjust < p.adjust_th) %>%
+  # slice_tail(n=20) %>%
+  ggplot(aes(x = NES, y = pathway, fill = padj)) +
+  geom_bar(stat = "identity") +
+  scale_fill_gradientn(
+    colours = c("red", "white"), guide = guide_colourbar(reverse = TRUE),
+    rescaler = ~ scales::rescale_mid(., mid = 0.05)
+  ) +
+  theme_bw() +
+  labs(title = "HALLMARK") +
+  ylab("")
+
+print(maurerbarplot_DEGEA_hallmark)
+# ggsave(maurerbarplot_DEGEA_hallmark,
+#        filename = paste0("results/Figures/maurer",subset_celltype ,"barplot_DEGEA_hallmark.svg"),
+#        width = 8,
+#        height = 5)
+
+
+# most correlated TF activities
+## Generate TF act
+### Select the regulon data
+data(dorothea_hs_pancancer, package = "dorothea")
+regulons <- dorothea_hs_pancancer %>%
+  dplyr::filter(confidence %in% c("A", "B", "C"))
+
+### VIPER Analysis
+################################################################################
+minsize <- 5
+ges.filter <- FALSE
+################################################################################
+tf_act__ <- dorothea::run_viper(counts, regulons,
+  options = list(
+    minsize = minsize, eset.filter = ges.filter,
+    cores = 1, verbose = FALSE, nes = TRUE
+  )
+)
+
+# Format for analysis
+tf_act_ <- as_tibble(tf_act__, rownames = "TF") %>%
+  pivot_longer(cols = -TF, names_to = "bcr_patient_barcode") %>%
+  pivot_wider(id_cols = c(bcr_patient_barcode), names_from = TF)
+
+### Visualisation
+#### Most variable TFs
+##### Calculate the n most variable TFs
+tf_50_var <- summarise(tf_act_, across(where(is.double), var)) %>%
+  pivot_longer(cols = everything(), names_to = "TF", values_to = "var") %>%
+  dplyr::arrange(desc(var)) %>%
+  dplyr::slice_head(n = 50)
+
+# Visualize TF activity
+# Create the matrix with only 50 top variable TF
+tf_matrix_var <- as_tibble(tf_act__, rownames = "TF") %>%
+  dplyr::select(TF, Maurer_PC1$sample) %>%
+  dplyr::filter(TF %in% tf_50_var$TF) %>%
+  column_to_rownames("TF") %>%
+  as.matrix()
+
+# Create the matrix with only 50 most correlated TF with PC1
+
+
+# Calculate absolute correlation for each gene
+tf_PC1_cor <- tf_act_ %>%
+  rename(sample = bcr_patient_barcode) %>%
+  inner_join(dplyr::select(Maurer_PC1, sample, PC1), by = "sample") %>% # make the two table in same order
+  arrange(PC1) %>%
+  dplyr::select(-PC1) %>%
+  column_to_rownames("sample") %>% # select continuous variables
+  as.matrix() %>%
+  cor(y = Maurer_PC1$PC1, method = "spearman") %>%
+  as.data.frame()
+
+# keep the 50 most absolute correlated TF
+top_tf <- mutate(tf_PC1_cor, V1 = abs(V1)) %>%
+  arrange(desc(V1)) %>%
+  slice(1:50) %>%
+  rename(cor = V1) %>%
+  rownames_to_column(var = "TF")
+
+# Final matrix
+tf_matrix_cor <- as_tibble(tf_act__, rownames = "TF") %>%
+  dplyr::select(TF, Maurer_PC1$sample) %>%
+  dplyr::filter(TF %in% top_tf$TF) %>%
+  column_to_rownames("TF") %>%
+  as.matrix()
+
+
+# Get a df of the variables I want to annotate the columns of the heatmap
+annotation_col <- dplyr::select(sample_info, Sample, PC1) %>%
+  column_to_rownames(var = "Sample")
+
+# Get a df of the variables I want to annotate the rows of the heatmap
+annotation_row_var <- tf_50_var %>%
+  column_to_rownames(var = "TF")
+
+# Choose the color scale for each annotation variable
+ann_colors <- list(
+  PC1 = c("white", "firebrick"),
+  PC1_weight = c("white", "darkorange"),
+  KRAS_VAF = c("white", "#0B2356"),
+  Moffitt = c(`BASAL-LIKE` = "#FF7F00", CLASSICAL = "#0000FC", `NA` = "grey"),
+  Bailey = c(ADEX = "#713E23", immunogenic = "#EF2C18", `NA` = "darkgrey", `pancreatic progenitor` = "#0000FC", squamous = "#FF7F00"),
+  Diabetes = c(no = "grey", yes = "black"),
+  var = c("white", "darkgreen"),
+  cor = brewer.pal(n = 11, name = "BrBG")
+)
+
+# Create breaks and limits for the continuous expression scale
+breaksList <- seq(-3.5, 3.5, by = 0.1)
+
+# Plot the final heatmap and pray so it works
+# Top variable tf
+pheatmap(tf_matrix_var,
+  annotation_col = annotation_col,
+  annotation_row = annotation_row_var,
+  annotation_colors = ann_colors, scale = "row", show_colnames = FALSE,
+  color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(length(breaksList)), # Defines the vector of colors for the legend (it has to be of the same lenght of breaksList)
+  breaks = breaksList,
+  fontsize = 12,
+  annotation_names_row = FALSE,
+  main = paste("Activity of the 50 most variable transcription factor on the Maurer", subset_celltype, "dataset", sep = " ")
+)
+
+# Get a df of the variables I want to annotate the rows of the heatmap
+annotation_row_cor <- tf_PC1_cor %>%
+  rownames_to_column("TF") %>%
+  inner_join(dplyr::select(top_tf, TF), by = "TF") %>%
+  rename(cor = V1) %>%
+  column_to_rownames(var = "TF")
+
+# Top correlated tf with PC1
+pheatmap(tf_matrix_cor,
+  annotation_col = annotation_col,
+  annotation_row = annotation_row_cor,
+  annotation_colors = ann_colors, scale = "row", show_colnames = FALSE,
+  color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(length(breaksList)), # Defines the vector of colors for the legend (it has to be of the same lenght of breaksList)
+  breaks = breaksList,
+  fontsize = 12,
+  annotation_names_row = FALSE,
+  main = paste("Activity of the 50 most correlated transcription factor with PC1 on the Maurer", subset_celltype, "dataset", sep = " ")
+)
